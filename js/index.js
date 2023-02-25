ccd4c417ca94db2111443985dc40a105ad7eea8a
Shanto Islam <rajs45693@gmail.com>
2023-02-26 01:32:05 +0600

    secend

--- js/index.js
+++ js/index.js
@@ -14,11 +14,14 @@ const imageFile = x => {
         const div = document.createElement('div');
         div.classList.add('img-store');
         div.innerHTML = `
-        <img class="img-size" src="${i.strMealThumb}">
-        <h4>card Taitle: ${i.strMeal}</h4>
-        <button onclick="lodeMeale()" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-        More info
-        </button>
+        <div class="card mt-5 shadow" style="width: 18rem;">
+            <img src="${i.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="">${i.strMeal}</h5>
+            <button onclick="lodeMeale(${i.idMeal})"  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            More info
+            </button>
+        </div>  
         `;
         imageContainar.appendChild(div);
     }
@@ -28,16 +31,27 @@ const serch = () => {
     const inputFild = document.getElementById('input-fild').value;
 
     lodeImage(inputFild);
-
 }
+lodeImage('')
 
-const lodeMeale = idMeal => {
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+const lodeMeale = (id) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => lodeSomthing(data.meals[0]));
+        .then(res => res.json())
+        .then(data => lodeSomthing(data.meals[0]));
+
 }
 const lodeSomthing = male => {
     document.getElementById('staticBackdropLabel').innerText = male.strMeal;
+    document.getElementById('meal-body').innerHTML = `
+    <div class="card" style="width:460px;">
+        <img src="${male.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${male.strMeal}</h5>
+            <p class="card-text">${male.strInstructions}</p>
+        </div>
+    </div>
+    `
+
+
 }
-lodeImage('fish')
